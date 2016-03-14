5bbf548c7680d80904f2fda4fb24cd13c6a94f2c
gabrie30 <jgabriels30@gmail.com>
2016-03-13 22:20:21 -0700

    add project descriptions

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,20 @@
 $(function() {
+
+  $('.jsa-image').hover(function(){
+    $('.jsa-project-description').fadeIn(800);
+  }, function(){
+    $('.jsa-project-description').hide();
+  });
+
+  $('.chess-image').hover(function(){
+    $('.chess-project-description').fadeIn(800);
+  }, function(){
+    $('.chess-project-description').hide();
+  });
+
+  $('.museseek-image').hover(function(){
+    $('.museseek-project-description').fadeIn(800);
+  }, function(){
+    $('.museseek-project-description').hide();
+  });
 });
