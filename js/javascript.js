30424b023e5c5315ff73eb7bf447c8d6727df313
JAG <JAG@JAGs-MacBook-Air.local>
2016-07-10 11:58:30 -0700

    add experience section with styling

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,33 @@
 $(function() {
 
   $('.jsa-image').hover(function(){
+
+    $('.chess-project-description').hide();
+    $('jsa-project-description').hide();
+    $('.museseek-project-description').hide();
+
     $('.jsa-project-description').fadeIn(800);
   }, function(){
     $('.jsa-project-description').hide();
   });
 
   $('.chess-image').hover(function(){
+
+    $('.chess-project-description').hide();
+    $('jsa-project-description').hide();
+    $('.museseek-project-description').hide();
+
     $('.chess-project-description').fadeIn(800);
   }, function(){
     $('.chess-project-description').hide();
   });
 
   $('.museseek-image').hover(function(){
+
+    $('.chess-project-description').hide();
+    $('jsa-project-description').hide();
+    $('.museseek-project-description').hide();
+
     $('.museseek-project-description').fadeIn(800);
   }, function(){
     $('.museseek-project-description').hide();
